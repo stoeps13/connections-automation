0b1edf495d51a87caa7b475f58263720068d6f03
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-11-21 11:09:11 +0100

    update run script to select environment and playbook as commandline option

--- runpb.sh
+++ runpb.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# Configure STDOUT_CALLBACK if -v is attached to the command
-# so the output is better readable during troubleshooting
-
-echo -n "$@" | grep -q -- "-v" && export ANSIBLE_STDOUT_CALLBACK=yaml
 
 # Check if commandline argument is given
 if [ $# -eq 0 ]; then
@@ -14,11 +10,53 @@ fi
 ANSIBLE_ENV=$1
 CMD_OPT="-i environments/${ANSIBLE_ENV}/inventory.ini --vault-password-file .vault-secret"
 
-ansible-playbook ${CMD_OPT} playbooks/setup-connections-complete.yml "$@"
-# ansible-playbook ${CMD_OPT} playbooks/third_party/setup-nginx.yml "$@"
-# ansible-playbook ${CMD_OPT} playbooks/third_party/setup-containerd.yml "$@"
-# ansible-playbook ${CMD_OPT} playbooks/setup-component-pack-complete-harbor.yml "$@"
+# ansible-playbook ${CMD_OPT} playbooks/setup-connections-complete.yml
+# ansible-playbook ${CMD_OPT} playbooks/third_party/setup-nginx.yml 
+# ansible-playbook ${CMD_OPT} playbooks/third_party/setup-containerd.yml
+# ansible-playbook ${CMD_OPT} playbooks/setup-component-pack-complete-harbor.yml
 # ansible-playbook ${CMD_OPT} playbooks/hcl/harbor/setup-deploy-apisix.yml
-# ansible-playbook ${CMD_OPT} playbooks/third_party/setup-tiny-editors.yml "$@"
-# ansible-playbook ${CMD_OPT} playbooks/hcl/setup-connections-docs.yml "$@"
+# ansible-playbook ${CMD_OPT} playbooks/third_party/setup-tiny-editors.yml
+# ansible-playbook ${CMD_OPT} playbooks/hcl/setup-connections-docs.yml
+# ansible-playbook ${CMD_OPT} playbooks/third_party/update-system.yml
+#
+printf "\nEnvironment: \t$1"
+printf "\nPlaybook: \t$2\n"
+
+case $2 in
+
+  apisix)
+    ansible-playbook ${CMD_OPT} playbooks/hcl/harbor/setup-deploy-apisix.yml
+    ;;
+
+  cnx)
+    ansible-playbook ${CMD_OPT} playbooks/setup-connections-complete.yml
+    ;;
+
+  componentpack)
+    ansible-playbook ${CMD_OPT} playbooks/setup-component-pack-complete-harbor.yml
+    ;;
+
+  docs)
+    ansible-playbook ${CMD_OPT} playbooks/hcl/setup-connections-docs.yml
+    ;;
+
+  osupdate)
+    ansible-playbook ${CMD_OPT} playbooks/third_party/update-system.yml
+    ;;
+
+  tiny)
+    ansible-playbook ${CMD_OPT} playbooks/third_party/setup-tiny-editors.yml
+    ;;
+
+  *)
+    printf "\nSelect a deployment\n"
+    printf "\tapisix         - Deploy new Api layer\n"
+    printf "\tcnx            - Deploy Connections complete\n"
+    printf "\tcomponentpack  - Deploy Componentpack latest\n"
+    printf "\tdocs           - Deploy CNX Docs\n"
+    printf "\tosupdate       - Update all OS packages\n"
+    printf "\ttiny           - Deploy Tiny Editors\n"
+    printf "\n"
+    ;;
+esac
 
